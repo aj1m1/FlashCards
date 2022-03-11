d5f61bd920038a687cdf52b4f4d29aa6557911b0
Jim Appiah <ajim@ad.unc.edu>
2022-03-10 22:19:19 -0500

    My Chemistry questions card

--- flashcards/ViewController.swift
+++ flashcards/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var frontLabel:UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+ 
+    @IBOutlet weak var card: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backLabel.isHidden = true
+        frontLabel.isHidden = false
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.8
+        
     }
-
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+//        frontLabel.isHidden = true;
+//        backLabel.isHidden = false;
+        frontLabel.isHidden.toggle()
+        backLabel.isHidden.toggle()
+        
+    }
+   
 }
 
